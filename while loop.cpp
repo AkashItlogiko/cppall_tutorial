3d91ba999797e38dcf6b78689f87c2af88edcf63
AkashItlogiko <139780035+AkashItlogiko@users.noreply.github.com>
2023-08-13 16:56:11 +0600

    Add files via upload

---
+++ while loop.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+main()
+{
+  int i=1;
+  while(i<=100)
+  {
+    cout<<"Bangladesh"<<endl;
+    i++;
+
+  }
+cout<<"End of while loop";
+ getch();
+
+
+
+
+    getch();
+}
